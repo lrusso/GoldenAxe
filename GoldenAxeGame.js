bb24785e20bcf701561326a3e1885e84b143bb3e
Leonardo Russo <info@lrusso.com>
2022-09-09 23:45:48 -0300

    Add files via upload

--- GoldenAxeGame.js
+++ GoldenAxeGame.js
@@ -558,7 +558,7 @@ GoldenAxe.Menu.prototype = {
 				{
 				var date = new Date();
 				date.setTime(date.getTime() + (days*24*60*60*1000));
-				expires = "; expires=" + date.toUTCString();
+				expires = "; expires=" + date.toUTCString() + "; SameSite=Lax";
 				}
 			document.cookie = name + "=" + (value || "")  + expires + "; Secure; path=/";
 			}
@@ -2287,7 +2287,7 @@ GoldenAxe.Game.prototype = {
 				{
 				var date = new Date();
 				date.setTime(date.getTime() + (days*24*60*60*1000));
-				expires = "; expires=" + date.toUTCString();
+				expires = "; expires=" + date.toUTCString() + "; SameSite=Lax";
 				}
 			document.cookie = name + "=" + (value || "")  + expires + "; Secure; path=/";
 			}
